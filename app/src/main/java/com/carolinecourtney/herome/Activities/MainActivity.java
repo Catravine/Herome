ab8fa07fdc541a3bff06117165008540b551a06d
Caroline Courtney <caroline@carolinecourtney.com>
2017-05-29 18:12:38 -0400

    Implementing FragmentManager

--- app/src/main/java/com/carolinecourtney/herome/Activities/MainActivity.java
+++ app/src/main/java/com/carolinecourtney/herome/Activities/MainActivity.java
@@ -1,15 +1,32 @@
 package com.carolinecourtney.herome.Activities;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 
 import com.carolinecourtney.herome.R;
+import com.carolinecourtney.herome.Fragments.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
     }
 }
